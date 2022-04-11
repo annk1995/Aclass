2ee6d47599becf4a03e154a5ac964b99c923771e
annk1995 <kabuikariuki052@gmail.com>
2022-04-11 23:27:12 +0300

    property binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MyComponentComponent } from './mycomponent/my-component/my-component.component';
+import { DataBindingComponent } from './databinding/data-binding/data-binding.component';
+import { PropertyBindingComponent } from './basics/property-binding/property-binding.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponentComponent,
+    DataBindingComponent,
+    PropertyBindingComponent
+  
   ],
   imports: [
     BrowserModule
